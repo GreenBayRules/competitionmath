afd72f986c6f5a0d9653f41101a3b2da76fc6c61
GreenBayRules <sheshank@sheshank.com>
2018-02-25 23:13:01 -0800

    d

--- scripts.js
+++ scripts.js
@@ -1,3 +1,47 @@
 function startLearning() {
   window.open("./guides.html");
 }
+
+
+function calculateAreaOfSquare() {
+  var side = document.getElementById("side").value;
+  if (isNaN(side)) {
+    var area = "Input Not Valid";
+  }
+  else {
+    var area = "A = " + side * side+ " units²";
+  }
+  document.getElementById("exact").innerHTML = area ;
+}
+
+
+
+
+
+/* For Circles */
+function calculateAreaOfCircle() {
+  var radius = document.getElementById("radius").value;
+  if (isNaN(radius)) {
+    var area = "Input Not Valid";
+    var areaWithPi = "Input Not Valid";
+  }
+  else {
+    var area = "A ≈ " + Math.PI * radius * radius + "" + " units²";
+    var areaWithPi = "A = " + radius * radius + "π" + " units²";
+  }
+  document.getElementById("exact").innerHTML = areaWithPi;
+  document.getElementById("approximate").innerHTML = area;
+}
+function calculateCircumfrence() {
+  var radius = document.getElementById("radius").value;
+  if (isNaN(radius)) {
+    var area = "Input Not Valid";
+    var areaWithPi = "Input Not Valid";
+  }
+  else {
+    var area = "C ≈ " + Math.PI * radius * 2 + "" + " units";
+    var areaWithPi = "C = " + radius * 2 + "π" + " units";
+  }
+  document.getElementById("exact").innerHTML = areaWithPi;
+  document.getElementById("approximate").innerHTML = area;
+}
